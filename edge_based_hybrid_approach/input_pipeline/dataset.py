ff6c251547ec8d953c22778d4ceba8f23fd2d3fd
Jiang241023 <jiangyueyang2023de@outlook.com>
2025-09-16 20:32:10 +0200

    add loops for getting all datasets

--- edge_based_hybrid_approach/input_pipeline/dataset.py
+++ edge_based_hybrid_approach/input_pipeline/dataset.py
@@ -16,32 +16,52 @@ dataset = DDACSDataset(data_dir, "h5")
 print(f"Loaded {len(dataset)} simulations")
 
 # Get a sample simulation
-sim_id, metadata, h5_path = dataset[20]
-print(f"Sample simulation: {sim_id}")
-print(f"File: {h5_path}")
-print(f"Metadata: {metadata}")
+for index in range(1): # If all datasets are needed, just set range(len(dataset))
+    sim_id, metadata, h5_path = dataset[index]
+    print(f"Sample simulation: {sim_id}")
+    print(f"File: {h5_path}")
+    print(f"Metadata: {metadata}")
 
-component = "blank"
-timestep = 3
+    # Load and analyze metadata
+    #df = dataset._metadata
+    #print(f"Metadata structure:")
+    #print(f"  Total simulations: {len(df)}")
+    #print(f"  Columns: {list(df.columns)}")
+    #print("  > Note: The column 'ID' is not returned in dataset.")
 
-with h5py.File(h5_path, "r") as f:
-    # choose "blank"
-    op10 = f["OP10"]
-    comp = op10[component]
+    # Show parameter meanings and ranges
+    #print(f"\nParameters (physical values):")
 
-    # coordinates
-    coords_all = comp["node_coordinates"][:] # [:] tells h5py to read the entire dataset from disk into a NumPy array  
-    print(f"the dimension of coords_all parameter is: {coords_all.ndim}")
-    coords_with_definite_timestep = coords_all
+    # Note: Update these when you have the actual unnormalized data
+    #desc = dataset.get_metadata_descriptions()
+    #for key in desc:
+    #    print(f"  > {key:4} - {desc[key]}")
 
-    # displacements
-    disp_all = comp["node_displacement"][:] # tells h5py to read the entire dataset from disk into a NumPy array
-    print(f"the dimension of disp_all parameter is: {disp_all.ndim}")    
-    disp_with_definite_timestep = disp_all[timestep]                  
-print(f"the shape of coords with timestep {timestep}:", coords_with_definite_timestep.shape)
-print(f"the shape of disp with timestep {timestep}:", None if disp_with_definite_timestep is None else disp_with_definite_timestep.shape)
+    #print(f"\nSample simulations:")
+    #print(df.head())
 
-# build features
-X_t = np.concatenate([coords_with_definite_timestep, disp_with_definite_timestep], axis=1)
-print("Vertex feature matrix shape:", X_t.shape)
-print("First 5 rows:\n", X_t[:5])
+    component = "blank"
+    timestep = 3
+
+    with h5py.File(h5_path, "r") as f:
+        # choose "blank"
+        op10 = f["OP10"]
+        comp = op10[component]
+
+        # coordinates
+        coords_all = comp["node_coordinates"][:] # [:] tells h5py to read the entire dataset from disk into a NumPy array  
+        print(f"the dimension of coords_all parameter is: {coords_all.ndim}")
+        coords_with_definite_timestep = coords_all
+
+        # displacements
+        disp_all = comp["node_displacement"][:] # tells h5py to read the entire dataset from disk into a NumPy array
+        print(f"the dimension of disp_all parameter is: {disp_all.ndim}")    
+        disp_with_definite_timestep = disp_all[timestep]
+
+    print(f"the shape of coords with timestep {timestep}:", coords_with_definite_timestep.shape)
+    print(f"the shape of disp with timestep {timestep}:", disp_with_definite_timestep.shape)
+
+    # build features
+    concatenated_representations = np.concatenate([coords_with_definite_timestep, disp_with_definite_timestep], axis=1)
+    print("Vertex feature matrix shape:", concatenated_representations.shape)
+    #print("First 5 rows:\n", concatenated_representations[:5])
