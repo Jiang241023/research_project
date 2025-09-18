27c0085dfa5189756e36f71a6b8df2664376ff21
Jiang241023 <jiangyueyang2023de@outlook.com>
2025-09-18 19:25:55 +0200

    add codes for Dataloader and representations of final coordinates and displacement

--- edge_based_hybrid_approach/input_pipeline/dataset.py
+++ edge_based_hybrid_approach/input_pipeline/dataset.py
@@ -6,10 +6,13 @@ from pathlib import Path
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from matplotlib import ticker
 import sys
-sys.path.append("../edge_based_hybrid_approach")
+sys.path.append("../research_project/edge_based_hybrid_approach")
 
 from DDACSDataset import DDACSDataset
 from utils.utils_DDACS import extract_point_cloud, extract_mesh, display_structure, extract_element_thickness, extract_point_springback
+
+from torch.utils.data import DataLoader, random_split
+import torch
 
 # Setup data directory
 data_dir = Path("/mnt/data/darus/")
@@ -38,7 +41,7 @@ for index in range(1): # If all datasets are needed, just set range(len(dataset)
     # Note: Update these when you have the actual unnormalized data
     #desc = dataset.get_metadata_descriptions()
     #for key in desc:
-    #    print(f"  > {key:4} - {desc[key]}")
+    #   print(f"  > {key:4} - {desc[key]}")
 
     #print(f"\nSample simulations:")
     #print(df.head())
@@ -68,7 +71,12 @@ for index in range(1): # If all datasets are needed, just set range(len(dataset)
         strain = comp["element_shell_strain"][:] 
         strain_at_definite_timestep = strain[timestep]
 
-    
+        # Get springback information
+        final_coords, displacement_vectors = extract_point_springback(h5_path, operation=10)
+
+
+    print(f"the shape of final_coords at timestep {timestep}:", final_coords.shape)
+    print(f"the shape of displacement_vectors at timestep {timestep}:", displacement_vectors.shape)
     print(f"the shape of coords at timestep {timestep}:", coords_at_definite_timestep.shape)
     print(f"the shape of disp at timestep {timestep}:", disp_at_definite_timestep.shape)
 
@@ -84,9 +92,28 @@ for index in range(1): # If all datasets are needed, just set range(len(dataset)
     print(f"the shape of strain at timestep {timestep}:", strain_at_definite_timestep.shape)
 
     # Example: get first element stress/strain
-    print("Stress first element:\n", stress_at_definite_timestep[0])
-    print("Strain first element:\n", strain_at_definite_timestep[0])
+    print("first element of Stress:\n", stress_at_definite_timestep[0])
+    print("first element of Strain:\n", strain_at_definite_timestep[0])
 
+    #Example: get 2 elements final_coords/displacement_vectors
+    print("elements of final_coords:\n", final_coords[:2])
+    print("elements of displacement_vectors:\n", displacement_vectors[:2])
+
+
+
+
+#ds = DDACSDataset(data_dir, "h5")
+
+# Always set before getting random operation
+torch.random.seed = 0
+train_fraction, test_fraction, eval_fraction = 0.7, 0.2, 0.1
+
+train_dataset, test_dataset, eval_dataset = random_split(dataset, [train_fraction, test_fraction, eval_fraction])
+print(f"Length of the datasets:\n - train: {len(train_dataset)}\n - test: {len(test_dataset)}\n - eval: {len(eval_dataset)}")
+
+train_dataloader = DataLoader(train_dataset, 16, shuffle=True)
+test_dataloader = DataLoader(test_dataset, 4, shuffle=True)
+eval_dataloader = DataLoader(eval_dataset, 4, shuffle=True)
 
 
 
